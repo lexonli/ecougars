fa91839e0d21966e261f69a01efeb6c55cdd677d
unknown <lexonli@hotmail.com>
2020-12-20 16:29:06 +1100

    Initial commit bitch

---
+++ app/src/main/java/com/deckle/ecougars/constants/ApiConstants.kt
@@ -0,0 +1,13 @@
+package com.deckle.ecougars.constants
+
+/** base url
+ * by default, this would be `http://10.0.2.2:8000` for calling your local development server
+ * but if you are calling a deployed server,
+ * eg. AWS: https://yx4k1wbmc7.execute-api.ap-southeast-2.amazonaws.com/api/list-cafes
+ */
+
+//Local server
+const val BASE_URL = "http://10.0.2.2:8000"
+
+//Development server
+//const val BASE_URL = "https://yx4k1wbmc7.execute-api.ap-southeast-2.amazonaws.com/api"
